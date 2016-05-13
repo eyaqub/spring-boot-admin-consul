ad300d15ae293b10873aaafc02d00f7439ef875b
Edwin Yaqub <EY@Edwins-MacBook-Pro.local>
2016-05-13 16:42:49 +0200

    updated dependencies (build.gradle) and enabled discovery in SpringBootAdminApplication

--- src/main/java/hello/SpringBootAdminApplication.java
+++ src/main/java/hello/SpringBootAdminApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
@@ -17,13 +22,14 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
  */
 @Configuration
 @EnableAutoConfiguration
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+//@EnableZuulProxy
 //@EnableAdminServer
+@SpringBootApplication
 public class SpringBootAdminApplication {
 	
     public static void main(String[] args) {
-        //SpringApplication.run(Application.class, args);
-        SpringApplication.run(SpringBootAdminApplication.class, args);
+      SpringApplication.run(SpringBootAdminApplication.class, args);
     }
     
 }
